1046aa0dc7a17ee6e639291ac1be6b16072a5f29
Chris Kaschner <chris.kaschner@gmail.com>
2016-08-23 11:04:12 -0500

    Add files for retrieving JSON and JQuery for getJSON

---
+++ app/static/retrieveJSON.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $('#get-data').click(function() {
+    var showImagesData = $('#show-images-data');
+
+
+    $.getJSON('http://127.0.0.1:5000/img/api/v1.0/images', function (data) {
+      console.log(data);
+
+      var items = data.images.map(function(item) {
+        return item.title + ': ' + item.url;
+      });
+      showImagesData.empty();
+
+      if (items.length) {
+        var content = '<li>' + items.join('</li><li>') + '</li>';
+        var list = $('<ul />').html(content);
+        showImagesData.append(list);
+      }
+    });
+
+    showImagesData.text('Loading the JSON file.');
+  })
+});
--- app/views.py
+++ app/views.py
@@ -1,6 +1,43 @@
+from flask import render_template, jsonify, abort, make_response, request, url_for
 from app import app
+
+jsonimages = [ #fake json array of images
+{
+  "url": "http://imgdirect.s3-website-us-west-2.amazonaws.com/nike.jpg",
+  "uri": "https://chriskaschner.pythonanywhere.com/img/api/v1.0/images/1",
+  "title": "Nikes"
+},
+{
+  "url": "http://imgdirect.s3-website-us-west-2.amazonaws.com/altra.jpg",
+  "uri": "https://chriskaschner.pythonanywhere.com/img/api/v1.0/images/2",
+  "title": "Altra"
+}
+]
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'ReturnPath'} # fake user
+
+    return render_template('index.html',
+                            title='Home',
+                            user=user,
+                            images=jsonimages)
+
+
+def make_public_img(image):
+    new_image={}
+    for field in image:
+        if field == 'id':
+            new_image['uri']= url_for('get_image', img_id=image['id'], _external=True)
+        else:
+            new_image[field] = image[field]
+    return new_image
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.route('/img/api/v1.0/images', methods=['GET'])
+def get_images():
+    return jsonify({'images': [make_public_img(image) for image in jsonimages]})
