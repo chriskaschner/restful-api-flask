01bfce5875865fe0141546e89d338502da9ab4ff
Chris Kaschner <chris.kaschner@gmail.com>
2016-10-19 09:53:43 -0500

    DELETE additional comments in app_test.py

--- test_app.py
+++ test_app.py
@@ -122,28 +122,3 @@ class AppTestCase(unittest.TestCase):
 
 if __name__ == '__main__':
         unittest.main()
-# @pytest.fixture
-# def client(request):
-#     db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-#     flaskr.app.config['TESTING'] = True
-#     client = flaskr.app.test_client()
-#     with flaskr.app.app_context():
-#         flaskr.init_db()
-#
-#     def teardown():
-#         os.close(db_fd)
-#         os.unlink(flaskr.app.config['DATABASE'])
-#     request.addfinalizer(teardown)
-#
-#     return client
-#
-#
-# def login(client, username, password):
-#     return client.post('/login', data=dict(
-#         username=username,
-#         password=password
-#     ), follow_redirects=True)
-#
-#
-# def logout(client):
-#     return client.get('/logout', follow_redirects=True)
