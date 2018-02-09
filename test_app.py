578425c7f77e4e350e3e47a351d8903b7d28024b
Chris Kaschner <chris.kaschner+github@gmail.com>
2018-02-09 14:57:07 -0600

    ADD Example files of redis based implementation

--- test_app.py
+++ test_app.py
@@ -3,11 +3,15 @@ import app
 import unittest
 import json
 import base64
-username = 'ReturnPath'
-password = 'python'
+username = 'ReturnPath'.encode("utf-8")
+password = 'python'.encode("utf-8")
+
+# User_Password_Encoded = base64.b64encode("{0}:{1}".format(username, password))
+User_Password_Encoded = ("{0}:{1}".format(username, password))
+User_Password_Encoded = User_Password_Encoded.encode()
 
 headers = {
-    'Authorization': 'Basic ' + base64.b64encode("{0}:{1}".format(username, password))
+    'Authorization': 'Basic ' + User_Password_Encoded
 }
 
 
